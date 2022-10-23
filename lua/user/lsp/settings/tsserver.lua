e54ea9f0ec2100c75489091aa84691f10b07b04e
renzhamin <renzhamin@gmail.com>
2022-10-23 17:14:33 +0600

    add RenamFile command with tsserver

--- lua/user/lsp/settings/tsserver.lua
+++ lua/user/lsp/settings/tsserver.lua
@@ -7,10 +7,49 @@ local function organize_imports()
     vim.lsp.buf.execute_command(params)
 end
 
+local function rename_file()
+    local source_file, target_file
 
+    vim.ui.input({
+        prompt = "Source : ",
+        completion = "file",
+        default = vim.api.nvim_buf_get_name(0)
+    },
+        function(input)
+            source_file = input
+        end
+    )
+    vim.ui.input({
+        prompt = "Target : ",
+        completion = "file",
+        default = source_file
+    },
+        function(input)
+            target_file = input
+        end
+    )
+
+    local params = {
+        command = "_typescript.applyRenameFile",
+        arguments = {
+            {
+                sourceUri = source_file,
+                targetUri = target_file,
+            },
+        },
+        title = ""
+    }
+
+    vim.lsp.util.rename(source_file, target_file)
+    vim.lsp.buf.execute_command(params)
+end
 
 return {
     commands = {
+        RenameFile = {
+            rename_file,
+            description = "Rename File"
+        },
         OrganizeImports = {
             organize_imports,
             description = "Organize Imports"
