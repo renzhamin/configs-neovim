316554a9188cdb7b2e7289c5e880022f2618dd48
renzhamin <renzhamin@gmail.com>
2023-04-20 12:35:08 +0600

    disable do not autostart eslintd

--- lua/user/plugins/null-ls.lua
+++ lua/user/plugins/null-ls.lua
@@ -9,8 +9,8 @@ end
 local M = {}
 
 M.sources = {
-    null_ls.builtins.code_actions.eslint_d,
-    null_ls.builtins.diagnostics.eslint_d,
+    --[[ null_ls.builtins.code_actions.eslint_d, ]]
+    --[[ null_ls.builtins.diagnostics.eslint_d, ]]
 }
 
 
