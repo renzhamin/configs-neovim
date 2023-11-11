bf1d37f7a01c7f2ce79656e7bda422f844522e50
renzhamin <renzhamin@gmail.com>
2023-11-11 20:06:39 +0600

    change harpoon keymaps

--- lua/user/keymaps/harpoon.lua
+++ lua/user/keymaps/harpoon.lua
@@ -12,8 +12,11 @@ ks('n', 'a', mark.add_file, { desc = "add current file" })
 ks('n', 'r', mark.rm_file, { desc = "remove current file" })
 ks('n', 'q', ui.toggle_quick_menu, { desc = "quick menu" })
 
-ks('n', 'n', ui.nav_next, { desc = "next marked file" })
-ks('n', 'p', ui.nav_prev, { desc = "previous marked file" })
+ks('n', ',', ui.nav_next, { desc = "next marked file" })
+ks('n', '.', ui.nav_prev, { desc = "previous marked file" })
+
+ks('n', ';', function() ui.nav_file(1) end,
+    { desc = "go to mark 1" })
 
 for i = 1, 9, 1
 do
@@ -21,7 +24,10 @@ do
         { desc = "go to mark " .. i })
 end
 
-for i, c in ipairs({ '!', '@', '#', '$' }) do
+ks('n', "'", function() term.gotoTerminal(1) end,
+    { desc = "go to terminal 1" })
+
+for i, c in ipairs({ '<F1>', '<F2>', '<F3>', '<F4>' }) do
     ks('n', c, function() term.gotoTerminal(i) end,
         { desc = "go to terminal" .. i })
 end
