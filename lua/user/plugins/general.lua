8b90b400ed340030cad02c5563cb551885e067d2
renzhamin <renzhamin@gmail.com>
2024-02-15 08:52:59 +0600

    added conflict-marker plugin

--- lua/user/plugins/general.lua
+++ lua/user/plugins/general.lua
@@ -144,5 +144,20 @@ return {
             },
         },
         main = "ibl",
+    },
+    {
+        'rhysd/conflict-marker.vim',
+        --[[ config = function() ]]
+        --[[     vim.g.conflict_marker_highlight_group = '' ]]
+        --[[     vim.g.conflict_marker_begin           = '^<<<<<<< .*$' ]]
+        --[[     vim.g.conflict_marker_end             = '^>>>>>>> .*$' ]]
+        --[[     vim.cmd [[ ]]
+        --[[         highlight ConflictMarkerBegin guibg=#2f7366 ]]
+        --[[         highlight ConflictMarkerOurs guibg=#2e5049 ]]
+        --[[         highlight ConflictMarkerTheirs guibg=#344f69 ]]
+        --[[         highlight ConflictMarkerEnd guibg=#2f628e ]]
+        --[[         highlight ConflictMarkerCommonAncestorsHunk guibg=#754a81 ]]
+        --[[     ]]
+        --[[ end ]]
     }
 }
